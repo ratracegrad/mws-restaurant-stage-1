7770a182d08f81060eb58645bbdb6fa45abeaa5a
Jennifer Bland <ratracegrad@gmail.com>
2018-09-26 07:34:00 -0400

    Added gulpfile to compress images and make images responsive by creating multiple sizes of images

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const responsive = require('gulp-responsive');
+
+gulp.task('compressImages', () => {
+    gulp.src('img/*.jpg')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img'));
+});
+
+gulp.task('responsiveImages', () => {
+    return gulp.src('img/*.jpg')
+        .pipe(responsive({
+            // Resize all JPG images to three different sizes: 200, 500, and 630 pixels
+            '*.jpg': [{
+                width: 200,
+                rename: { suffix: '-200px' },
+            }, {
+                width: 500,
+                rename: { suffix: '-500px' },
+            }, {
+                width: 630,
+                rename: { suffix: '-630px' },
+            }, {
+                // Compress, strip metadata, and rename original image
+                rename: { suffix: '-original' },
+            }],
+        }, {
+            // Global configuration for all images
+            // The output quality for JPEG, WebP and TIFF output formats
+            quality: 70,
+            // Use progressive (interlace) scan for JPEG and PNG output
+            progressive: true,
+            // Strip all metadata
+            withMetadata: false,
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['compressImages', 'responsiveImages']);
