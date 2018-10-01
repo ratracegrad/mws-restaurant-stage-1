979dc72060d6422636a052f19c8418015eea3758
Jennifer Bland <ratracegrad@gmail.com>
2018-10-01 06:38:03 -0400

    Updated to use fetch to retrieve restaurants from server

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-/* eslint-disable no-unused-vars */
 let restaurants,
     neighborhoods,
     cuisines;
-var map;
+var newMap;
 var markers = [];
-/* eslint-enable no-unused-vars */
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (event) => {
+    initMap(); // added
     fetchNeighborhoods();
     fetchCuisines();
     addServiceWorker();
@@ -71,20 +70,37 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 };
 
 /**
- * Initialize Google map, called from HTML.
+ * Initialize leaflet map, called from HTML.
  */
-window.initMap = () => {
-    let loc = {
-        lat: 40.722216,
-        lng: -73.987501,
-    };
-    self.map = new google.maps.Map(document.getElementById('map'), {
+initMap = () => {
+    self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
         zoom: 12,
-        center: loc,
-        scrollwheel: false,
+        scrollWheelZoom: false,
     });
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1IjoicmF0cmFjZWdyYWQiLCJhIjoiY2ptbnh0bDNjMHNxazNwazR0MDA5Z3UzdiJ9.uBPRKglrewvJzjy3qkqRjA',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets',
+    }).addTo(newMap);
+
     updateRestaurants();
 };
+/* window.initMap = () => {
+  let loc = {
+    lat: 40.722216,
+    lng: -73.987501
+  };
+  self.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: loc,
+    scrollwheel: false
+  });
+  updateRestaurants();
+} */
 
 /**
  * Update page and map for current restaurants.
@@ -119,7 +135,9 @@ resetRestaurants = (restaurants) => {
     ul.innerHTML = '';
 
     // Remove all map markers
-    self.markers.forEach(m => m.setMap(null));
+    if (self.markers) {
+        self.markers.forEach(marker => marker.remove());
+    }
     self.markers = [];
     self.restaurants = restaurants;
 };
@@ -173,10 +191,11 @@ createRestaurantHTML = (restaurant) => {
 addMarkersToMap = (restaurants = self.restaurants) => {
     restaurants.forEach(restaurant => {
         // Add marker to the map
-        const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-        google.maps.event.addListener(marker, 'click', () => {
-            window.location.href = marker.url;
-        });
+        const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+        marker.on("click", onClick);
+        function onClick() {
+            window.location.href = marker.options.url;
+        }
         self.markers.push(marker);
     });
 };
